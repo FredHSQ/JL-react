da65195e7b86cad12dd8a1d2f264d90e52b4d448
Danilo Silva de Oliveira <danilo.silva.oliveira0021@gmail.com>
2021-06-22 13:10:46 -0300

    Começando a paginação

---
+++ src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Login.module.css';
+import Header from '../Header/Header';
+import NavBar from '../NavBar/NavBar';
+
+const Login = () => (
+  <>
+    <Header/>
+    <NavBar/>
+  </>
+);
+
+Login.propTypes = {};
+
+Login.defaultProps = {};
+
+export default Login;
