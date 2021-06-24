8afb59d9b7b97cb4b287410408e4929300149448
Danilo Silva de Oliveira <danilo.silva.oliveira0021@gmail.com>
2021-06-24 13:54:26 -0300

    Aprimorando pagina de portal

--- src/components/Crud/Crud.js
+++ src/components/Crud/Crud.js
@@ -1,22 +1,88 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Crud.module.css';
 import Categoria from '../Categoria/Categoria'
 import Cliente from '../Cliente/Cliente';
 import Funcionario from '../Funcionario/Funcionario'
 import Produto from '../Produto/Produto'
+import { useHistory } from 'react-router-dom'
 
-const Crud = () => (
-  <div className={styles.Crud}>
-    <Cliente/>
-    <Categoria/>
-    <Funcionario/>
-    <Produto/>
+import './Crud.css';
+
+const { useState } = React;
+
+const tabItems = [
+  {
+    id: 1,
+    title: 'Cliente',
+    content: <Cliente/>,
+  },
+  {
+    id: 2,
+    title: 'Funcionário',
+    content: <Funcionario/>,
+  },
+  {
+    id: 3,
+    title: 'Categoria',
+    content: <Categoria/>,
+  },
+  {
+    id: 4,
+    title: 'Produto',
+    content: <Produto/>,
+  },
+];
+
+const Crud = () => {
+ const [active, setActive] = useState(0);
+  
+  return (
+  <div className='Crud'>
+    <div className="wrapper">
+      <div className="tabs">
+        {tabItems.map(({ id, title }) =><TabItemComponent
+           key={title}
+           title={title}
+           onItemClicked={() => setActive(id)}
+           isActive={active === id}
+         />
+      )}
+      </div>
+      <div className="content">
+        {tabItems.map(({ id, content }) => {
+          return active === id ? content : ''
+        })}
+      </div>
+     </div>
   </div>
-);
+  )
+}
 
-Crud.propTypes = {};
+const TabItemComponent = ({
+  title = '',
+  onItemClicked = () => console.error('You passed no action to the component'),
+  isActive = false,
+}) => {
+  return (
+    <div className={isActive ? 'tabitem' : 'tabitem tabitem--inactive'} onClick={onItemClicked}>
+      <p className="tabitem__title">{title}</p>
+    </div>
+  )
+};
 
-Crud.defaultProps = {};
+
+
+// const Crud = () => (
+//   <div className={styles.Crud}>
+//     <Button className={styles.Button} value='Cliente' selected={true}></Button>
+//     <Button className={styles.Button} value='Categoria' selected={false}></Button>
+//     <Button className={styles.Button} value='Funcionário' selected={false}></Button>
+//     <Button className={styles.Button} value='Produto' selected={false}></Button>
+
+//     <Cliente/>
+//     <Categoria/>
+//     <Funcionario/>
+//     <Produto/>
+//   </div>
+// );
 
 export default Crud;
