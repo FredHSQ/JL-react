2d790a8025082fe630afe17859d243013fb9833a
Frederico Sheremetieff <frederico.shere@gmail.com>
2021-06-25 14:53:38 -0300

    Commit responsividade header e footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Footer.module.css';
-import Logo from '../../Assets/liga.svg'
+import Logo from '../../Assets/liga.svg';
+import styled from 'styled-components';
+
+const imagemFooter = ({className, children}) => (
+  <img className={className} src={Logo} alt="Logo" id="img-funko">
+    {children}
+  </img>
+)
+const Imagem = styled(imagemFooter)`
+width: 80vw;`
+
 
 const Footer = () => (
   <footer className={styles.Footer}>
-    <img src={Logo} alt="Logo" className={styles.mediaLogo} />
+    <Imagem />
   </footer>
 );
 
